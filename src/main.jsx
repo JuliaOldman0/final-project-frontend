a2e525c7c05c60218e5eb2d2ad142b37e3fb893b
Julia Oldman <78836093+JuliaOldman0@users.noreply.github.com>
2026-05-19 11:47:08 -0400

    Add header styles, fonts, and hero background

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import "./vendor/fonts.css";
 import "./index.css";
 import App from "./components/App/App.jsx";
 
